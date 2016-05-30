32641a9626c1a5b71c451f6fab4a9ff793a9cc91
ruoyu0088 <ruoyu0088@gmail.com>
2016-05-30 20:05:16 +0900

    add theme selector

--- scpy3/style.js
+++ scpy3/style.js
@@ -1,8 +1,46 @@
 (function(){
-var imports, load;
+var _pyfunc_contains = function contains (a, b) { // nargs: 2
+    if (b == null) {
+    } else if (Array.isArray(b)) {
+        for (var i=0; i<b.length; i++) {if (_pyfunc_equals(a, b[i]))
+                                           return true;}
+        return false;
+    } else if (b.constructor === Object) {
+        for (var k in b) {if (a == k) return true;}
+        return false;
+    } else if (b.constructor == String) {
+        return b.indexOf(a) >= 0;
+    } var e = Error('Not a container: ' + b); e.name='TypeError'; throw e;
+};
+var _pyfunc_equals = function equals (a, b) { // nargs: 2
+    if (a == null || b == null) {
+    } else if (Array.isArray(a) && Array.isArray(b)) {
+        var i = 0, iseq = a.length == b.length;
+        while (iseq && i < a.length) {iseq = equals(a[i], b[i]); i+=1;}
+        return iseq;
+    } else if (a.constructor === Object && b.constructor === Object) {
+        var akeys = Object.keys(a), bkeys = Object.keys(b);
+        akeys.sort(); bkeys.sort();
+        var i=0, k, iseq = equals(akeys, bkeys);
+        while (iseq && i < akeys.length) {k=akeys[i]; iseq = equals(a[k], b[k]); i+=1;}
+        return iseq;
+    } return a == b;
+};
+var _pyfunc_truthy = function (v) {
+    if (v === null || typeof v !== "object") {return v;}
+    else if (v.length !== undefined) {return v.length ? v : false;}
+    else if (v.byteLength !== undefined) {return v.byteLength ? v : false;} 
+    else {return Object.getOwnPropertyNames(v).length ? v : false;}
+};
+var _pymeth_append = function (x) { // nargs: 1
+    if (!Array.isArray(this)) return this.append.apply(this, arguments);
+    this.push(x);
+};
+var imports, load, themes;
 imports = ["base/js/namespace", "require"];
+themes = ["default", "oceans16", "grade3", "space-legos"];
 load = function (Jupyter, require) {
-    var load_css, main;
+    var get_metadata, load_css, main, set_metadata, unload_css;
     load_css = (function (name) {
         var link;
         link = document.createElement("link");
@@ -13,8 +51,91 @@ load = function (Jupyter, require) {
         return null;
     }).bind(this);
 
+    unload_css = (function (names) {
+        var dummy1_sequence, dummy2_iter, dummy3_sequence, dummy4_sequence, dummy5_iter, el, href, name, to_remove;
+        to_remove = [];
+        dummy3_sequence = document.getElementsByTagName("link");
+        for (el in dummy3_sequence) {
+            if (!dummy3_sequence.hasOwnProperty(el)){ continue; }
+            el = dummy3_sequence[el];
+            href = el.getAttribute("href");
+            dummy1_sequence = names;
+            if ((typeof dummy1_sequence === "object") && (!Array.isArray(dummy1_sequence))) {
+                dummy1_sequence = Object.keys(dummy1_sequence);
+            }
+            for (dummy2_iter = 0; dummy2_iter < dummy1_sequence.length; dummy2_iter += 1) {
+                name = dummy1_sequence[dummy2_iter];
+                if (((_pyfunc_truthy(_pyfunc_contains("scpy3", href))) && (_pyfunc_truthy(_pyfunc_contains(name, href))))) {
+                    _pymeth_append.call(to_remove, el);
+                    break;
+                }
+            }
+        }
+        dummy4_sequence = to_remove;
+        if ((typeof dummy4_sequence === "object") && (!Array.isArray(dummy4_sequence))) {
+            dummy4_sequence = Object.keys(dummy4_sequence);
+        }
+        for (dummy5_iter = 0; dummy5_iter < dummy4_sequence.length; dummy5_iter += 1) {
+            el = dummy4_sequence[dummy5_iter];
+            el.parentNode.removeChild(el);
+        }
+        return null;
+    }).bind(this);
+
+    get_metadata = (function (target, key) {
+        var meta;
+        meta = target.metadata;
+        if (_pyfunc_truthy(!_pyfunc_contains("scpy3", meta))) {
+            return null;
+        }
+        if (_pyfunc_truthy(!_pyfunc_contains(key, meta.scpy3))) {
+            return null;
+        }
+        return meta.scpy3[key];
+    }).bind(this);
+
+    set_metadata = (function (target, key, value) {
+        var meta;
+        meta = target.metadata;
+        if (_pyfunc_truthy(!_pyfunc_contains("scpy3", meta))) {
+            meta.scpy3 = {};
+        }
+        meta.scpy3[key] = value;
+        return null;
+    }).bind(this);
+
     load_css("./style.css");
     main = (function () {
+        var dummy6_sequence, dummy7_iter, on_theme_changed, select, theme;
+        on_theme_changed = (function () {
+            var theme;
+            theme = select.val();
+            unload_css(themes.slice(1));
+            if ((!_pyfunc_equals(theme, "default"))) {
+                load_css("./themes/" + theme + ".css");
+            }
+            set_metadata(Jupyter.notebook, "theme", theme);
+            return null;
+        }).bind(this);
+
+        select = jQuery("<select/>").attr("id", "scpy3-theme-selector");
+        select.addClass("form-control select-xs");
+        _pymeth_append.call(select, jQuery("<optgroup label = \"Themes:\">"));
+        dummy6_sequence = themes;
+        if ((typeof dummy6_sequence === "object") && (!Array.isArray(dummy6_sequence))) {
+            dummy6_sequence = Object.keys(dummy6_sequence);
+        }
+        for (dummy7_iter = 0; dummy7_iter < dummy6_sequence.length; dummy7_iter += 1) {
+            theme = dummy6_sequence[dummy7_iter];
+            _pymeth_append.call(select, ((jQuery("<option/>").attr("value", theme)).text(theme)));
+        }
+        select.change(on_theme_changed);
+        _pymeth_append.call(Jupyter.toolbar.element, select);
+        theme = get_metadata(Jupyter.notebook, "theme");
+        if ((theme !== null)) {
+            select.val(theme);
+            on_theme_changed();
+        }
         return null;
     }).bind(this);
 
--- scpy3/style.py
+++ scpy3/style.py
@@ -1,11 +1,31 @@
 imports = ['base/js/namespace', 'require']
 
+themes = ['default', 'oceans16', 'grade3', 'space-legos']
+
 def load(Jupyter, require):
-    from .utils import load_css
+    from .utils import load_css, unload_css, get_metadata, set_metadata
     load_css('./style.css')
-    
+
     def main():
-        pass
+        def on_theme_changed():
+            theme = select.val()
+            unload_css(themes[1:])
+            if theme != 'default':
+                load_css('./themes/%s.css' % theme)
+            set_metadata(Jupyter.notebook, 'theme', theme)
+                
+        select = jQuery('<select/>').attr('id', 'scpy3-theme-selector')
+        select.addClass('form-control select-xs')
+        select.append(jQuery('<optgroup label = "Themes:">'))
+        for theme in themes:
+            select.append(jQuery('<option/>').attr('value', theme).text(theme))
+        select.change(on_theme_changed)
+        Jupyter.toolbar.element.append(select)
+
+        theme = get_metadata(Jupyter.notebook, 'theme')
+        if theme is not None:
+            select.val(theme)
+            on_theme_changed()
     
     return {"load_ipython_extension": main}
 
