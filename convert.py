e4557260a6be400dd215ca1854ae1ad698e84da2
ruoyu0088 <ruoyu0088@gmail.com>
2016-06-11 22:39:33 +0900

    use ast to parse utils imports

--- convert.py
+++ convert.py
@@ -56,21 +56,46 @@ def callback(match, fn_py):
     return "\n\n".join(textwrap.indent(func, indent)
                        for func in get_functions(utils_fn, functions))
 
+def replace_utils_re(code):
+    return re.sub(r'^( *)from \.utils import (.+)',
+           lambda match:callback(match, fn_py),
+                     code, flags=re.MULTILINE)
+
+def replace_utils_ast(fn_py, code):
+    utils_fn = path.join(path.dirname(fn_py), 'utils.py')
+    root = ast.parse(code)
+    all_nodes = list(ast.walk(root))
+    del_lines = set()
+    to_insert = {}
+    code_lines = code.split('\n')
+    for i, node in enumerate(all_nodes):
+        if isinstance(node, ast.ImportFrom) and node.module == 'utils':
+            del_lines.update(range(node.lineno, all_nodes[i+1].lineno))
+            functions = [o.name for o in node.names]
+            functions_code = "\n".join(get_functions(utils_fn, functions))
+            line = code_lines[node.lineno - 1]
+            indent = len(line) - len(line.lstrip())
+            to_insert[node.lineno] = textwrap.indent(functions_code, " " * indent)
+    res = []
+    for i, line in enumerate(code.split('\n'), 1):
+        if i in to_insert:
+            res.append(to_insert[i])
+        if i not in del_lines:
+            res.append(line)
+    return "\n".join(res)
+        
 for fn_py in glob("*/*.py"):
     if fn_py.endswith("utils.py"):
         continue
     
     fn_js = fn_py.replace(".py", ".js")
-    if not path.exists(fn_js) or os.stat(fn_js).st_mtime < os.stat(fn_py).st_mtime:
+    if True or not path.exists(fn_js) or os.stat(fn_js).st_mtime < os.stat(fn_py).st_mtime:
         print(fn_py)
 
         with open(fn_py) as f:
             code_py = f.read()
 
-        code_py = re.sub(r'^( *)from \.utils import (.+)',
-               lambda match:callback(match, fn_py),
-                         code_py, flags=re.MULTILINE)
-
+        code_py = replace_utils_ast(fn_py, code_py)
         code_js = py2js(code_py, inline_stdlib=True)
 
         with open(fn_js, "wb") as f:
